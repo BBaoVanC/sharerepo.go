ce9154fb6389c2893610a066584dc8f9a0f8eab9
BBaoVanC <bbaovanc@bbaovanc.com>
2021-12-20 10:46:54 -0600

    Add v2

--- sharerepo.go
+++ sharerepo.go
@@ -13,7 +13,6 @@ type RepoPageData struct {
 
 func main() {
     http.HandleFunc("/", handler)
-    http.HandleFunc("/picker", handlePicker)
 
     staticBox := rice.MustFindBox("static")
     staticFileServer := http.StripPrefix("/static/", http.FileServer(staticBox.HTTPBox()))
@@ -25,53 +24,48 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    tmplBox, err := rice.FindBox("templates")
-    if err != nil {
-        log.Fatal(err)
-    }
+    switch r.URL.Path {
+    case "/picker":
+        renderTemplate("picker.html", RepoPageData{}, w, r)
+    case "/v2":
+    case "/v2/":
+        renderTemplate("v2.html", RepoPageData{}, w, r)
+    default:
+        log.Println("Processing a request")
+        repos, ok := r.URL.Query()["repo"]
+        if !ok || len(repos[0]) < 1 {
+            log.Println("Missing repo parameter, redirecting to picker")
+            http.Redirect(w, r, "/picker", 303)
+            return
+        }
 
-    tmplString, err := tmplBox.String("repo.html")
-    if err != nil {
-        log.Fatal(err)
-    }
+        repo := repos[0]
+        data := RepoPageData {
+            URL: repo,
+        }
+        log.Println("Repo is", repo)
 
-    log.Println("Processing a request")
-    repos, ok := r.URL.Query()["repo"]
-    if !ok || len(repos[0]) < 1 {
-        log.Println("Missing repo parameter, redirecting to picker")
-        http.Redirect(w, r, "/picker", 303)
-        return
+        renderTemplate("repo.html", data, w, r)
     }
-
-    repo := repos[0]
-    data := RepoPageData {
-        URL: repo,
-    }
-    log.Println("Repo is", repo)
-    tmpl, err := template.New("repo").Parse(tmplString)
-    if err != nil {
-        log.Fatal(err)
-    }
-    tmpl.Execute(w, data)
 }
 
-func handlePicker(w http.ResponseWriter, r *http.Request) {
+func renderTemplate(tmplFile string, data RepoPageData, w http.ResponseWriter, r *http.Request) {
     tmplBox, err := rice.FindBox("templates")
     if err != nil {
 	log.Fatal(err)
     }
 
-    tmplString, err := tmplBox.String("picker.html")
+    tmplString, err := tmplBox.String(tmplFile)
     if err != nil {
             log.Fatal(err)
     }
 
-    tmpl, err := template.New("picker").Parse(tmplString)
+    tmpl, err := template.New(tmplFile).Parse(tmplString)
     if err != nil {
             log.Fatal(err)
     }
 
-    tmpl.Execute(w, nil)
+    tmpl.Execute(w, data)
 }
 
 func handleNoContent(w http.ResponseWriter, r *http.Request) {
